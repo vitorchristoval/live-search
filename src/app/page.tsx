2feb35c577ab9b16aee8ff7a9e57d8430320c085
Vitor Christoval <vitor.christoval@gmail.com>
2025-01-25 18:36:09 -0300

    update envs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import SearchInput from "@/components/SearchInput";
 
 
 export default function Home() {
+  console.log(process.env.API_KEY)
   return (
     <div className="flex align-middle justify-center items-center h-auto  max-w-prose">
         <SearchInput/>
--- src/hooks/useApiServices.tsx
+++ src/hooks/useApiServices.tsx
@@ -4,7 +4,7 @@ export const useApiServices = () => {
     const request = axios.create({
         baseURL: 'https://api.themoviedb.org/3/',
         params: {
-            api_key: '890580ebea281a7322da2043724cab0f'
+            api_key: process.env.API_KEY
         }
     });
     request.interceptors.response.use((response) => response, (error) => {
